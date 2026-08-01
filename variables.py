1f21fd3e0d59de958c9a6676e749dd5b1f2236e7
yair shvili <yairyshvili@gmail.com>
2026-08-01 08:58:17 +0300

    Add variables and data types practice

---
+++ variables.py
@@ -0,0 +1,12 @@
+ticker ="AAPL"
+price = 227.5
+shares = 100
+total_value = price * shares
+print (total_value)
+print("stock:" ,ticker)
+print ("total value:", total_value)
+print(type(ticker))
+print(type(price))
+print(type(shares))
+result = str(shares) + ticker
+print(result)
